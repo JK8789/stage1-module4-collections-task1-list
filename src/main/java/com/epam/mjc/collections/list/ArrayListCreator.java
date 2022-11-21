79561988cc57a22881496efad3437360d8ea9411
Korotkova Julia <korotkovayuliav@gmail.com>
2022-11-21 16:04:21 +0100

    done

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -1,9 +1,29 @@
 package com.epam.mjc.collections.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> list = new ArrayList<>();
+
+        ListIterator<String> it = sourceList.listIterator(0);
+        while (it.hasNext()) {
+            if ((it.nextIndex() + 1) % 3 == 0) {
+                String nextWord = it.next();
+                list.add(nextWord);
+                list.add(nextWord);
+            } else it.next();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ArrayListCreator arrayListCreator = new ArrayListCreator();
+        List<String> source = Arrays.asList("The", "ArrayList", "class", "has", "many", "useful", "methods");
+        System.out.println("Source List: " + source);
+        System.out.println("New List: " + arrayListCreator.createArrayList(source));
     }
 }
--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> newList = new LinkedList<>();
+        sourceList.forEach((element) ->
+        {
+            if (element % 2 != 0) {
+                newList.addFirst(element);
+            } else {
+                newList.addLast(element);
+            }
+        });
+        return newList;
     }
 }
